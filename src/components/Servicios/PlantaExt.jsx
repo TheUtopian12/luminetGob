236b380be61382d5e6a1c725fb320fd1ecc3d1c1
TheUtopian12 <edwi.castruita117@gmail.com>
2023-01-06 12:37:46 -0600

    Se agrego servicios

--- src/components/Servicios/PlantaExt.jsx
+++ src/components/Servicios/PlantaExt.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logoL from "../../assets/cofetel.jpg";
+import logoL from "../../assets/Cofetel.jpg";
 export const PlantaExt = () => {
   return (
     <div className="flex flex-col pl-20">
