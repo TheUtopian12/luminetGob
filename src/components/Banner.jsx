2091a75d2178087b704bff7dc95d17680c76a03f
TheUtopian12 <edwi.castruita117@gmail.com>
2023-01-10 18:52:43 -0600

    Se agrego componente de Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,20 +1,88 @@
 import React from "react";
 import bgImage from "../assets/banner.jpg";
+
+import Slider from "react-slick";
 export const Banner = () => {
+  const settings = {
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    arrows: false,
+  };
   return (
     <div
       className={`w-[100%] h-[300px] bgImageBanner bg-cover flex items-center justify-center`}
     >
       <div className="text-white text-center ml-5 mr-5">
         <h1 className="text-lg font-bold sm:text-3xl">¿Por qué luminet?</h1>
-        <h2 className="text-lg sm:text-3xl">
-          Ofrecemos
-          <span className="text-[#76BA22] font-semibold">
+        <div className="flex justify-center">
+          {" "}
+          <div className="w-[1200px]">
             {" "}
-            soporte operativo y monitoreo
-          </span>{" "}
-          24/7 de la red de Fibra Óptica propia y externa
-        </h2>
+            <Slider {...settings}>
+              <div>
+                <h2 className="text-lg sm:text-3xl">
+                  Ofrecemos
+                  <span className="text-[#76BA22] font-semibold">
+                    {" "}
+                    soporte operativo y monitoreo
+                  </span>{" "}
+                  24/7 de la red de Fibra Óptica propia y externa
+                </h2>
+              </div>
+
+              <div>
+                <h2 className="text-lg sm:text-3xl">
+                  Porque somos{" "}
+                  <span className="text-[#76BA22] font-semibold">
+                    caso de éxito
+                  </span>{" "}
+                  en la operación y mantenimiento de la{" "}
+                  <span className="text-[#76BA22] font-semibold">
+                    red de videogilancia
+                  </span>{" "}
+                  de Monterrey
+                </h2>
+              </div>
+
+              <div>
+                <h2 className="text-lg sm:text-3xl">
+                  Brindamos{" "}
+                  <span className="text-[#76BA22] font-semibold">
+                    consultoría especializada
+                  </span>{" "}
+                  para mejorar el desempeño de la red, reubicación de sitios y
+                  cambios de topología de la red{" "}
+                </h2>
+              </div>
+
+              <div>
+                <h2 className="text-lg sm:text-3xl">
+                  Porque somos una empresa que tiene{" "}
+                  <span className="text-[#76BA22] font-semibold">
+                    amplia experiencia{" "}
+                  </span>{" "}
+                  para
+                  <span className="text-[#76BA22] font-semibold">
+                    operar redes propias y externas
+                  </span>{" "}
+                  , que se especializa en planta interna y planta externa{" "}
+                </h2>
+              </div>
+
+              <div>
+                <h2 className="text-lg sm:text-3xl">
+                  Porque al elegirnos contarás con una empresa con más de{" "}
+                  <span className="text-[#76BA22] font-semibold">
+                    23 años de experiencia
+                  </span>{" "}
+                  en el ramo, con personal técnico capacitado y con las
+                  herramientas requeridas para un plan de remediación
+                </h2>
+              </div>
+            </Slider>
+          </div>
+        </div>
       </div>
     </div>
   );
