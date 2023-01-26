1cbea1f0095a40104738fb154a65b949e5fb577f
TheUtopian12 <edwi.castruita117@gmail.com>
2023-01-26 15:29:57 -0600

    Se agrego navbar

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <>
     <a href='#/'
-        title="Contacto"
-        className=" group fixed z-50 top-[200px] right-10 bg-[#43BA22] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#71b55e]   duration-300"
+        title="Inicio"
+        className=" group fixed z-50 top-[200px] right-10 bg-[#000E20] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#000E20]   duration-300"
       >
         <span className="group-hover:flex hidden">
           {" "}
@@ -15,29 +15,30 @@ const Navbar = () => {
       </a> 
 
       <a href='#ventajas'
-        title="Contacto"
-        className=" group fixed z-50 top-[250px] right-10 bg-[#43BA22] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#71b55e]   duration-300"
+        title="Ventajas"
+        className=" group fixed z-50 top-[250px] right-10 bg-[#03152D] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#03152D]   duration-300"
       >
         <span className="group-hover:flex hidden">
           {" "}
-          <h1 className="text-white text-[12px]">Ventajas</h1>
+          <h1 className="text-white text-[12px]">Nosotros</h1>
         </span>
         <span className="group-hover:hidden"><BiTrendingUp size={25}/></span>
       </a> 
       <a href='#servicios'
-        title="Contacto"
-        className=" group fixed z-50 top-[300px] right-10 bg-[#43BA22] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#71b55e]   duration-300"
+        title="Servicios"
+        className=" group fixed z-50 top-[300px] right-10 bg-[#0A3F73] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#0A3F73]   duration-300"
       >
         <span className="group-hover:flex hidden">
           {" "}
-          <h1 className="text-white text-[12px]">Servicios</h1>
+          <h1 className="text-white text-[12px]">Servicios 
+</h1>
         </span>
         <span className="group-hover:hidden"><BiBroadcast size={25}/></span>
       </a> 
 
       <a href='#clientes'
-        title="Contacto"
-        className=" group fixed z-50 top-[350px] right-10 bg-[#43BA22] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#71b55e]   duration-300"
+        title="Clientes"
+        className=" group fixed z-50 top-[350px] right-10 bg-[#0D488C] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#0D488C]   duration-300"
       >
         <span className="group-hover:flex hidden">
           {" "}
@@ -47,8 +48,8 @@ const Navbar = () => {
       </a> 
 
       <a href='#polizas'
-        title="Contacto"
-        className=" group fixed z-50 top-[400px] right-10 bg-[#43BA22] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#71b55e]   duration-300"
+        title="Polizas"
+        className=" group fixed z-50 top-[400px] right-10 bg-[#3970AF] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#3970AF]   duration-300"
       >
         <span className="group-hover:flex hidden">
           {" "}
@@ -57,20 +58,11 @@ const Navbar = () => {
         <span className="group-hover:hidden"><BiSpreadsheet size={25}/></span>
       </a> 
 
-      <a href='#tecnologias'
-        title="Contacto"
-        className=" group fixed z-50 top-[450px] right-10 bg-[#43BA22] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#71b55e]   duration-300"
-      >
-        <span className="group-hover:flex hidden">
-          {" "}
-          <h1 className="text-white text-[12px]">Tecnologías </h1>
-        </span>
-        <span className="group-hover:hidden"><BiChip size={25}/></span>
-      </a>
+      
 
       <a href='#contacto'
         title="Contacto"
-        className=" group fixed z-50 top-[500px] right-10 bg-[#43BA22] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#71b55e]   duration-300"
+        className=" group fixed z-50 top-[450px] right-10 bg-[#6FB1FD] w-10 h-10 hover:w-32 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#6FB1FD]   duration-300"
       >
         <span className="group-hover:flex hidden">
           {" "}
