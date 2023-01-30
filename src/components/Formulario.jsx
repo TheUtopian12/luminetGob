37acd9c35643658410f42e6e7c38425b8e274e2e
TheUtopian12 <edwi.castruita117@gmail.com>
2023-01-30 14:34:28 -0600

    Se agrego formulario responsivo

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -6,7 +6,10 @@ import { BiRightArrow } from "react-icons/bi";
 import { MdArrowForwardIos } from "react-icons/md";
 const Formulario = () => {
   return (
-    <div className="min-h-screen bg-[#95C926] flex flex-col place-items-center" id="contacto">
+    <div
+      className="min-h-screen bg-[#95C926] flex flex-col place-items-center"
+      id="contacto"
+    >
       <h1 className="sm:text-5xl text-[#143C6A] font-bold text-center pt-10">
         Solicitar más información
       </h1>
@@ -15,13 +18,13 @@ const Formulario = () => {
         contacte a la brevedad y te brinde la información detallada.
       </h2>
       <div className="hidden relative top-80 w-12 h-12 bg-white rounded-full sm:flex justify-center items-center">
-        <MdArrowForwardIos size={30} className="text-[#95C926] font-bold"/>
+        <MdArrowForwardIos size={30} className="text-[#95C926] font-bold" />
       </div>
-      <div className="w-[100%] sm:w-[60%] min-h-screen pb-10  pt-20 rounded-2xl grid sm:grid-cols-2 gap-10 sm:gap-0">
-        <div className="h-[70vh] w-[100%] sm:w-[100%] bg-gradient-to-b from-[#266095] to-[#031e3d] sm:rounded-l-3xl hidden sm:flex flex-col place-items-center pt-5">
+      <div className="w-[100%] sm:w-[60%] min-h-screen pb-10  pt-20 rounded-2xl grid sm:grid-cols-2 gap-0 sm:gap-0">
+        <div className="ml-5 sm:ml-0 h-[40vh] sm:h-[70vh] w-[90%] sm:w-[100%] bg-gradient-to-b from-[#266095] to-[#031e3d] rounded-t-3xl sm:rounded-tr-none sm:rounded-l-3xl flex flex-col place-items-center pt-5">
           <img src={Logo} alt="" className="w-32" />
-          <div className=" flex flex-col text-white m-5">
-            <div className="flex gap-5">
+          <div className=" flex flex-col text-white sm:m-5">
+            <div className="hidden sm:flex gap-5">
               <SlLocationPin size={50} className="text-[#95C926]" />
               <p>
                 <h2>Pedro Figueroa No. 2815 CP 25024 Saltillo, Coahuila</h2>
@@ -32,7 +35,7 @@ const Formulario = () => {
                 </h2>
               </p>
             </div>
-            <div className="flex gap-5 mt-5">
+            <div className="hidden sm:flex gap-5 mt-5">
               <SlEnvolope size={33} className="text-[#95C926]" />
               <p>
                 <h2>contacto@luminetgobierno.com.mx</h2>
@@ -48,27 +51,28 @@ const Formulario = () => {
             </div>
           </div>
         </div>
-        <div className="ml-5 sm:ml-0 h-[70vh] w-[90%] sm:w-[100%] bg-[#e3ebf2] sm:rounded-r-2xl sm:rounded-l-none  rounded-3xl">
+
+        <div className="ml-5 sm:ml-0 h-[70vh] w-[90%] sm:w-[100%] bg-[#e3ebf2] sm:rounded-r-2xl sm:rounded-l-none rounded-b-3xl">
           <div className="flex flex-col justify-center items-center place-items-center m-8 gap-10">
             <Input
               variant="outlined"
               label="Nombre"
               icon={<BiRightArrow />}
-              color="cyan"
+              color="blue"
             />
             <Input
               variant="outlined"
               label="E-mail"
               icon={<BiRightArrow />}
-              color="cyan"
+              color="blue"
             />
             <Input
               variant="outlined"
               label="Teléfono"
               icon={<BiRightArrow />}
-              color="cyan"
+              color="blue"
             />
-            <Textarea label="Mensaje" color="cyan" />
+            <Textarea label="Mensaje" color="blue" />
 
             <button
               type="button"
