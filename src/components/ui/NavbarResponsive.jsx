3be8822d8d7d37aeb261962813f32025563cb8d4
TheUtopian12 <edwi.castruita117@gmail.com>
2023-01-30 15:34:15 -0600

    Se agrego formulario responsivo

--- src/components/ui/NavbarResponsive.jsx
+++ src/components/ui/NavbarResponsive.jsx
@@ -8,6 +8,8 @@ import {
   BiSpreadsheet,
   BiChip,
   BiConversation,
+  BiX,BiXCircle,
+  BiMenu
 } from "react-icons/bi";
 const NavbarResponsive = () => {
   return (
@@ -40,10 +42,8 @@ const NavbarResponsive = () => {
 
       <div id="floating-button">
         <p className="plus">+</p>
-        <img
-          className="edit"
-          src="https://ssl.gstatic.com/bt/C3341AA7A1A076756462EE2E5CD71C11/1x/bt_compose2_1x.png"
-        />
+        
+        <BiXCircle size={30} className="edit text-white"/>
       </div>
     </div>
   );
