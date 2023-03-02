57b40b29236b4ad97c8597444969df786fe46421
TheUtopian12 <edwi.castruita117@gmail.com>
2023-03-02 17:59:31 -0600

    Se cambio texto de hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,11 +26,10 @@ export const Hero = () => {
       <div className="absolute w-full h-full top-0 left-0 bg-gray-900/30"></div>
       <div className="absolute top-0 w-full h-full flex flex-col justify-center text-center text-white">
         <h1 className="text-2xl mr-1 ml-1 sm:mr-10 sm:ml-10 sm:text-5xl font-bold 4k:text-6xl lg:text-white  2xl:mr-16 2xl:ml-16">
-          Servicios de integración, consultoría y proyectos especiales para
-          sector público{" "}
+          Servicios de redes y seguridad para el sector público{" "}
         </h1>
         <h2 className="text-xl mr-5 ml-5 sm:ml-96 sm:mr-96 sm:text-3xl mt-10 text-[#DFFFD3] 4k:text-5xl">
-          Integramos servicios especializados para una conectividad y seguridad
+          Brindamos servicios especializados para una conectividad y seguridad
           total
         </h2>
         <div className="mt-10">
